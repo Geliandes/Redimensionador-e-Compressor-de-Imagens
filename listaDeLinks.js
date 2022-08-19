31c13566ed19767ec32ec391b4b2c3ec3ff62f4e
Paulo Geliandes <88293654+Geliandes@users.noreply.github.com>
2022-08-18 22:13:05 -0300

    Delete listaDeLinks.js

--- listaDeLinks.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-var array = fs.readFileSync('insercao de links.txt').toString().replace(/(\r\n|\n|\r)/gm, "").split(",");
-
-const textoFinal = {
-    arrayDeLinks: array
-}
-console.log(textoFinal.arrayDeLinks)
-
-module.exports = textoFinal
