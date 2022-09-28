202d0e168c79f3dfd6ab45e2d04e4244d353491b
Paulo Geliandes <geliandes@hotmail.com>
2022-09-28 10:46:57 -0300

    update webp compressor

--- webpCompress.js
+++ webpCompress.js
@@ -2,83 +2,48 @@ const mainApp = require('./app');
 
 let dir = 'resized'
 
-if (mainApp.width == null && mainApp.height == null) {
+if(mainApp.width == null && mainApp.height == null) {
 	dir = 'converted'
 }
 
 function webpCompress() {
+	const fs = require('fs');
 	const sharp = require('sharp');
 	const fsp = require('fs/promises');
 	const path = require('path');
-
-	fsp.readdir(path.join(`./${dir}`))
-		.then(resized => {
-			const images = resized.filter((image) => true);
-
-			for (let i = 0; i < images.length; i++) {
-				sharp(input = `./${dir}/${images[i]}`)
-					.webp({
-						lossless: false,
-						alphaQuality: 100,
-						quality: mainApp.qualidade,
-						force: true
-					})
-					.toFile('./compressed/' + images[i].toString(), (err, info) => {
-						if (err) {
-							console.log(`Não foi possível comprimir a imagem ${images[i]}`);
+	fsp.readdir(path.join(`./${dir}`)).then(async(resized) => {
+		const images = resized.filter((image) => true);
+		for(let i = 0; i < images.length; i++) {
+			await sharp(input = `./${dir}/${images[i]}`).webp({
+				lossless: false,
+				alphaQuality: 100,
+				quality: mainApp.qualidade,
+				force: true
+			}).toFile('./compressed/' + images[i].toString()).then((info, err) => {
+				if(err) {
+					console.log(`Não foi possível comprimir a imagem ${images[i]}`);
+				} else {
+					fs.stat(`./${dir}/${images[i]}`, (err, stats) => {
+						if(err) {
+							console.log(`O arquivo não existe`)
+						} else {
+							let oldSize = ((stats.size) / 1000).toFixed(2)
+							let newSize = ((info.size) / 1000).toFixed(2)
+							let percentVariationSize = ((newSize * 100) / (oldSize) - 100).toFixed(2)
+							if(newSize < oldSize) {
+								console.log("\033[31mATENÇÃO!" + "\033[0m o tamanho da imagem " + images[i] + " aumentou " + "\033[31m+" + percentVariationSize + "%" + "\033[0m")
+							} else {
+								console.log("\033[0;32mATENÇÃO!" + "\033[0m o tamanho da imagem " + images[i] + " diminuiu " + "\033[0;32m" + percentVariationSize + "%" + "\033[0m")
+							}
+							console.log(`Tamanho antigo: ${oldSize} KB`)
+							console.log(`Novo comprimido: ${newSize} KB`)
+							console.log('')
 						}
 					})
-			}
-		})
-}
-/*
-                else{
-                    setTimeout(() => {                 
-                        oldSize(images, i);
-                        newSize(images, i);
-                    }, 5000);
-                    setTimeout(()=>{
-                        oldSize(images, i);
-                        newSize(images, i);
-
-                        if(newSize(images, i) < oldSize(images, i)){
-                            let increased = ((newSize(images, i) * 100) / (oldSize(images, i)) - 100).toFixed(2)
-                            console.log("\033[31mATENÇÃO!" +"\033[0m o tamanho da imagem " + images[i] + " aumentou " + "\033[31m+" + increased + "%" +"\033[0m")
-                        } else{
-                            let decreased = ((newSize(images, i) * 100) / (oldSize(images, i)) - 100).toFixed(2)
-                            console.log("\033[0;32mATENÇÃO!" +"\033[0m o tamanho da imagem " + images[i] + " diminuiu " + "\033[0;32m" + decreased + "%" +"\033[0m")
-                        }
-                    },10000) 
-                }
-            });
-        }
-    })
+				}
+			})
+		}
+	})
 }
 
-function oldSize(images, i){
-    const fs = require('fs');
-    const path = require('path');
-
-    fs.stat(path.join(`./${dir}/${images[i]}`), (err, stats) => {
-        if(err){
-            console.log(err)
-        }
-        globalThis.oldSizeValue = (stats.size/1000).toFixed(2);
-    })
-    return globalThis.oldSizeValue
-}
-
-
-function newSize(images, i){
-    const fs = require('fs');
-    const path = require('path');
-    fs.stat(path.join(`./compressed/${images[i]}`), (err, stats) => {
-        if(err){
-            console.log(err)
-        }
-        globalThis.newSizeValue = (stats.size/1000).toFixed(2);
-    })
-    return globalThis.newSizeValue
-}
-*/
 module.exports = webpCompress;
